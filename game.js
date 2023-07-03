110119ef99fa6594e3b8e6f1e7773c5e19f00a66
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-07-02 21:09:11 -0300

    feat(game.js): create snake

--- game.js
+++ game.js
@@ -1,6 +1,21 @@
+function initSnake(length, headPosition) {
+    return [...Aray(length).keys()].map((index) => {
+        return {
+            x: headPosition.x + index,
+            y: headPosition.y,
+        };
+    });
+}
+
 function gameFunction() {
     const gameDisplay = document.getElementById("game-display");
     const FPS = parseInt(1000 / 30);
+    const gridLength = 20;
+    const cellSize = 20;
+    let snakeSections = initSnake(5, {
+        x: gridLength / 2,
+        y: gridLength / 2,
+    });
 
     function render() {
         //clear HTML elements
